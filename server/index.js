300f44b89b2101bace8f7c4202236870e3a74ea3
Ran1t <ranitlaha00@gmail.com>
2026-09-05 11:11:21 +0530

    endpoint name reverted

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ mongoose.connect(process.env.dbUrl).then(() => {
 
 app.use(express.json())
 app.use(cookieParser())
-app.use("/customers",userRoutes)
+app.use("/users",userRoutes)
 
 
 app.listen(PORT,()=>{
